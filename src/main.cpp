a95961058fb77c5c4af65bb05c003b17787732d6
Piotr Piatkowski <pp@idea7.pl>
2022-12-08 22:40:40 +0100

    Sleep mode and low power mode added - not tested yet

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,18 @@
 #define NLEDS 16
 #define PIN_LED 4
 #define PIN_BTN 3
+#define PIN_VOLTAGE A1
 
 Adafruit_NeoPixel pixels(NLEDS, PIN_LED, NEO_RGB + NEO_KHZ800);
-
-long lastClick = 0;
 
 const int MIN_CLICK_DELAY = 200;
 const int MODES_NUM = 5;
 int currentMode = 0;
+bool lowPowerMode = false;
+bool sleepMode = false;
+long lastClick = 0;
+const long SLEEP_TIME = 1000l * 30 * 60; // 30 minutes
+const int MIN_POWER_LEVEL = 500; // TODO check voltage
 
 #define RANDOM_COLORS_LEN 20
 uint32_t randomColors[RANDOM_COLORS_LEN];
@@ -39,6 +43,7 @@ void setup() {
             b = random(128);
         randomColors[i] = pixels.Color(r, g, b);
     }
+    lastClick = millis(); // don't go into sleep mode too fast
 }
 
 int led_row(int pnum) {
@@ -67,11 +72,11 @@ uint32_t getColor(int mode, long now, int pnum) {
     double v2 = (sin(now / 1000.0 + pnum + 3) + 1.0) / 2.0;
     int row = led_row(pnum);
     switch(mode) {
-        // red/orange slow change
-        case 4:
-            if (v2 > v1 / 2)
-                v2 = v1 / 2;
-            return pixels.Color(v1 * 128, v2 * 64, 0);
+        // random colors, changing each second
+        case 0: {
+            int offset = ((now / 1000) + pnum * 13) % RANDOM_COLORS_LEN;
+            return randomColors[offset];
+        }
         // blue/green slow change
         case 1:
             return pixels.Color(0, v1 * 128, v2 * 128);
@@ -96,40 +101,85 @@ uint32_t getColor(int mode, long now, int pnum) {
             else
                 return pixels.Color(0, 0, 0);
         }
-        // random colors, changing each second
-        case 0: {
-            int offset = ((now / 1000) + pnum * 13) % RANDOM_COLORS_LEN;
-            return randomColors[offset];
-        }
+        // red/orange slow change
+        case 4:
+            if (v2 > v1 / 2)
+                v2 = v1 / 2;
+            return pixels.Color(v1 * 128, v2 * 64, 0);
         default:
             return pixels.Color(0, 0, 0);
     }
+}
+
+bool powerLevelCorrect() {
+    int power_level = analogRead(4);
+    return power_level >= MIN_POWER_LEVEL;
 }
 
 void loop() {
 
     long now = millis();
 
-    // TODO - check voltage and disable if too low
-
     if (!digitalRead(PIN_BTN)) {
         if (now - lastClick > MIN_CLICK_DELAY) {
             currentMode++;
-            if (currentMode >= MODES_NUM)
-                currentMode = 0;
-            for (int i = 0; i < 3; i++) {
-                pixels.clear();
-                pixels.fill(pixels.Color(0, 128, 0), 0, currentMode + 1);
-                pixels.show();
-                delay(300);
-                pixels.clear();
-                pixels.show();
-                delay(200);
+            if (lowPowerMode) {
+                // just reset it
+                lowPowerMode = false;
+            }
+            else if (sleepMode) {
+                // just reset it
+                sleepMode = false;                
+            }
+            else {
+                if (currentMode >= MODES_NUM)
+                    currentMode = 0;
+                for (int i = 0; i < 3; i++) {
+                    pixels.clear();
+                    pixels.fill(pixels.Color(0, 128, 0), 0, currentMode + 1);
+                    pixels.show();
+                    delay(300);
+                    pixels.clear();
+                    pixels.show();
+                    delay(200);
+                }
             }
         }
         lastClick = now;
-
     }
+
+    if(lowPowerMode)
+        return;
+
+    int power_level = analogRead(PIN_VOLTAGE);
+    if(power_level < MIN_POWER_LEVEL) {
+        for(int i = 0; i < 3; i++) {
+            pixels.fill(pixels.Color(64, 0, 0));
+            pixels.show();
+            delay(300);
+            pixels.clear();
+            pixels.show();
+            delay(300);
+            lowPowerMode = true;
+            return;
+        }
+    }
+
+    if (now - lastClick > SLEEP_TIME) {
+        sleepMode = true;
+    }
+
+    if(sleepMode) {
+        pixels.clear();
+        if ((now / 1000) % 4 == 0) {
+            pixels.setPixelColor(0, pixels.Color(50, 0, 0));
+            pixels.setPixelColor(1, pixels.Color(50, 0, 0));
+        }
+        pixels.show();
+        delay(100); // save power
+        return;
+    }
+
         
     for(int i = 0; i < NLEDS; i++)
         pixels.setPixelColor(i, getColor(currentMode, now, i));
